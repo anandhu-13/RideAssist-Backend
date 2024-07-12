2f1a013ffd97d9e9c734d9aec2d20d16fee77005
anandhu <anandhu13vijayan@gmail.com>
2024-07-12 15:36:33 +0530

    first insertion

---
+++ api/views.py
@@ -0,0 +1,53 @@
+from rest_framework.response import Response
+
+from api.models import Customer,Work
+
+from api.serializers import CustomerSerializer,WorkSerializer
+
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+
+from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+
+
+class CustomerListCreateView(ListAPIView, CreateAPIView):
+
+    authentication_classes=[JWTAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+
+    serializer_class = CustomerSerializer
+
+    queryset = Customer.objects.all()
+
+
+class CustomerRetrieveUpdateDestroyView(RetrieveAPIView, UpdateAPIView, DestroyAPIView):
+
+    authentication_classes=[JWTAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+
+    serializer_class = CustomerSerializer
+
+    queryset = Customer.objects.all()
+    
+
+
+class WorkCreateView(CreateAPIView):
+
+    permission_classes=[permissions.IsAuthenticated]
+
+    authentication_classes=[JWTAuthentication]
+
+    serializer_class=WorkSerializer
+
+    queryset=Work.objects.all()
+
+
+    def perform_create(self, serializer):
+
+        id=self.kwargs.get("pk")
+
+        instance=Customer.objects.get(id=id)
+
+        serializer.save(customer_object=instance)
